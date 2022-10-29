5fc28b51be2c03f5f26aa13cd238b343755d4c68
Nightwind416 <christopher.e.lorr@gmail.com>
2022-10-29 08:11:36 -0500

    Resource update and work on pseudocode

---
+++ project.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def main():
+    ...
+
+
+
+def function_1():
+    ...
+
+
+def function_2():
+    ...
+
+
+def function_n():
+    ...
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_project.py
@@ -0,0 +1,16 @@
+from project import test_function_1
+from project import test_function_2
+from project import test_function_3
+
+
+
+def test_function_1():
+    ...
+
+
+def test_function_2():
+    ...
+
+
+def test_function_n():
+    ...
