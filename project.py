79d15bc5e03f240be9940bfb9e4adad8c8f6ec51
Nightwind416 <christopher.e.lorr@gmail.com>
2022-11-27 23:42:16 -0600

    More overall code work

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
+import random
+
+# Standard inputs for AREPL, remove or comment out when compiling
+standard_input = ["3", "clue"]
 
 
 
-
-# Create a list of sleuth cards
+# List of sleuth cards
 sleuth_cards = [
     'Take another Turn',
     'Take another Turn',
@@ -38,8 +41,19 @@ sleuth_cards = [
     'Back to the Acme Detective Agency (You or another Detective)',
     ]
 
+# List of detective IDs
+detective_ids = [
+    "Mavis Marvel",
+    "Kent Ketchum",
+    "Harley Hand",
+    "Carrie Badger",
+    "Rosa Subrosa",
+    "Sheerluck Holmes",
+    "Lester Lose O'",
+    "Nanny Harrow",
+    ]
 
-# Create a dictionary of wanted names and values
+# Ddictionary of wanted names and values
 wanted_list = {
     "Armand Slinger": 900,
     "Bunny & Clod": 1000,
@@ -54,8 +68,11 @@ wanted_list = {
     }
 
 
-# Create a thief class
-# thief has name, wanted value, captured status, additional crimes, location
+# Player move list
+move_list = ["clue", "roll", "arrest", "use sleuth card", "end turn"]
+
+
+# Thief class - has name, wanted value, captured status, additional crimes, location
 class thief:
     def __init__(self, name, location):
         self.name = name
@@ -63,18 +80,49 @@ class thief:
         self.additional_crimes = 0
         self.succesful_escapes = 0
         self.location = location
-        self.final_bounty = 0
-        self.arresting_detective = "None"
+        self.bounty = 0
+        self.arresting_detective = None
     def __str__(self):
         return self.name
-    def __repr__(self):
-        return self.name
-    def __eq__(self, other):
-        return self.name == other
+    # Increase bounty by amount
+    def increase_bounty(self, amount):
+        self.bounty += amount
+    # Increase crime count
+    def increase_crimes(self):
+        self.additional_crimes += 1
+    # Increase escape count
+    def increase_escapes(self):
+        self.succesful_escapes += 1
+    # Set location
+    def set_location(self, location):
+        self.location = location
+    # Set captured status
+    def set_captured_status(self, status):
+        self.captured_status = status
+    # Set arresting detective
+    def set_arresting_detective(self, detective):
+        self.arresting_detective = detective
+    # Get current bounty
+    def get_bounty(self):
+        return self.bounty
+    # Get crime count
+    def get_crimes(self):
+        return self.additional_crimes
+    # Get escape count
+    def get_escapes(self):
+        return self.succesful_escapes
+    # Get current location
+    def get_location(self):
+        return self.location
+    # Get captured status
+    def get_captured_status(self):
+        return self.captured_status
+    # Get arresting detective
+    def get_arresting_detective(self):
+        return self.arresting_detective
 
 
-# Create a player class
-# player has a detective name, list of held cards, current winnings, arrested thieves list
+# Player class - has a detective name, list of held cards, current winnings, arrested thieves list
 class player:
     def __init__(self, name):
         self.name = name
@@ -83,13 +131,6 @@ class player:
         self.arrested_thieves = []
     def __str__(self):
         return self.name
-    def __repr__(self):
-        return self.name
-    def __eq__(self, other):
-        return self.name == other
-
-
-
 
 
 
@@ -98,7 +139,18 @@ def main():
     new_game()
     # Begin a new round of the game
     new_round()
-
+    # Game loop
+    while True:
+        # Check if thief is captured or not
+        # If thief is captured, end round
+        # If thief is not captured, continue
+        # Loop through each player
+        for player in players:
+            # Call player turn function
+            player_turn(player)
+            # Check if thief is captured or not
+            # If thief is captured, end round
+            # If thief is not captured, continue
 
 
 
@@ -106,13 +158,52 @@ def main():
 def new_game():
     # Ask for the number of players
     players = int(input("How many players?"))
-    # Each player chooses a detective ID
+    print("There are " + str(players) + " players.")
+    # Loop through each player choosing a detective ID and add to a player list
+    for i in range(players):
+        # Choose a detective ID
+        detective = random.choice(detective_ids)
+        # Remove detective ID from list
+        detective_ids.remove(detective)
+        # Create a new player
+        new_player = player(detective)
+        # Add new player to player list
+        players.append(new_player)
+        # Print new player
+        print("Player " + str(i + 1) + ": " + new_player.name)
+        print("Winnings: " + str(new_player.winnings))
+        print("Arrested Thieves: " + str(new_player.arrested_thieves))
+        print("Held Cards: " + str(new_player.held_cards))
 
 
 
-# def new_round():
-#     # Pick a random thief
+def new_round():
+    # Create a new thief from the wanted list
+    new_thief = thief(random.choice(list(wanted_list.keys())), "placeholder_location")
+    print("The new thief is " + new_thief.name + "!")
+    print("Name: " + new_thief.name)
+    print("Captured Status: " + str(new_thief.captured_status))
+    print("Additional Crimes: " + str(new_thief.additional_crimes))
+    print("Succesful Escapes: " + str(new_thief.succesful_escapes))
+    print("Location: " + new_thief.location)
+    print("Current Bounty: " + str(new_thief.bounty))
+    print("Arresting Detective: " + str(new_thief.arresting_detective))
 
+
+
+
+
+def player_turn(player):
+    # Ask for player if ready to take their turn
+    # ready_check = input("Player " + player.name + ", are you ready for your clue?")
+    player_input = input("What would you like to do?")
+    print("You chose to " + player_input + ".")
+    # If player input is to move
+    # If player input is to sleuth
+    # If player input is to arrest
+    # If player input is to end turn
+    # If player input is to end game
+    return
 
 
 
